f0a9f5742bf3cd8b9f2c8d39f7d5e3fee45476bc
Mateus <mateus.frauzino@gmail.com>
2023-08-15 12:23:41 -0300

    removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {  Blog, Features, Footer, Header, Possibility, WhatGPT3,} from './containers';
-import { Brand, CTA, Navbar } from './components';
+import {  Footer, Header } from './containers';
+import { Navbar } from './components';
 import './App.css';
 
 const App = () => {
