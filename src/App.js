0882e515829c67cdc4e5d45fbc66f05f8a7621ba
Mateus <mateus.frauzino@gmail.com>
2023-08-18 10:24:40 -0300

    adding social media icons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import {  Footer, Header } from './containers';
+import { Footer, Header } from './containers';
 import { Navbar } from './components';
 import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="bg">
+    <div className='App'>
+      <div className='bg'>
         <Header />
         <Navbar />
       </div>
+      <div className='footer-div'>
+        <Footer />
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './footer.css';
-import logo from '../../assets/caliandra-logo.png'
+import whatsapp_icon from '../../assets/whatsapp_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
 
 const Footer = () => {
   return (
     <div className='caliandra__footer'>
-        <img className='caliandra__footer-img' src={logo} alt="caliandra-logo"/>
+      <a href='https://wa.me/5561991892606' target='blank'>
+        <img
+          className='caliandra__footer-whatsapp-icon'
+          src={whatsapp_icon}
+          alt='whatsapp icon'
+        />
+      </a>
+      <a href='https://instagram.com/caliandra.escola' target='blank'>
+        <img
+          className='caliandra__footer-instagram-icon'
+          src={instagram_icon}
+          alt='instagram icon'
+        />
+      </a>
     </div>
   );
 };
