e472a77673a8a517ef360627219e44cdcf354721
Mateus <mateus.frauzino@gmail.com>
2023-08-18 12:23:31 -0300

    adding content to 'agende-uma-visita' page

--- src/pages/agende-uma-visita/Agende-uma-visita.jsx
+++ src/pages/agende-uma-visita/Agende-uma-visita.jsx
@@ -8,6 +8,27 @@ const AgendeUmaVisita = () => {
     <div className='AgendeUmaVisita'>
       <Header />
       <Navbar />
+      <div className='AgendeUmaVisita_mainContent'>
+        <h1 className='AgendeUmaVisita_h1'>Agende uma visita</h1>
+        <div className='tabela-visitas'>
+          <table>
+            <tr>
+              <td>Segunda-feira</td>
+              <td>Terça-feira</td>
+              <td>Quarta-feira</td>
+              <td>Quinta-feira</td>
+            </tr>
+            <tr>
+              <td>15h às 17h</td>
+              <td>15h às 17h</td>
+              <td>15h às 17h</td>
+              <td>15h às 17h</td>
+            </tr>
+          </table>
+        </div>
+        <p>*Verificar disponibilidade via WhatsApp</p>
+      </div>
+
       <div className='footer-div'>
         <Footer />
       </div>
