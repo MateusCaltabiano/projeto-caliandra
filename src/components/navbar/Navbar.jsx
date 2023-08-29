85f91b8930aeaf698974e574cfbf8bb91d3d73c8
Mateus <mateus.frauzino@gmail.com>
2023-08-29 17:20:52 -0300

    navbar: changing routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,20 +5,34 @@ import './navbar.css';
 
 const Navbar = () => {
   return (
-      <div className="caliandra__navbar">
-        <div className="caliandra__navbar-links">
-          <div className="caliandra__navbar-links_container">
-            <p><a href="/">Home</a></p>
-            <p><a href="QuemSomos">Quem somos</a></p>
-            <p><a href="NossaProposta">Nossa Proposta</a></p>
-            <p><a href="NossoEspaco">Nosso Espaço</a></p>
-            <p><a href="NossaEquipe">Nossa equipe</a></p>
-            <p><a href="Contato">Contato</a></p>
-            <p><a href="AgendeUmaVisita">Agende uma visita</a></p>
-          </div>
+    <div className='caliandra__navbar'>
+      <div className='caliandra__navbar-links'>
+        <div className='caliandra__navbar-links_container'>
+          <p>
+            <a href='/projeto-caliandra'>Home</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/QuemSomos'>Quem somos</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/NossaProposta'>Nossa Proposta</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/NossoEspaco'>Nosso Espaço</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/NossaEquipe'>Nossa equipe</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/Contato'>Contato</a>
+          </p>
+          <p>
+            <a href='/projeto-caliandra/AgendeUmaVisita'>Agende uma visita</a>
+          </p>
         </div>
       </div>
-  )
+    </div>
+  );
 };
 
 export default Navbar;
