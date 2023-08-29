6001f729f01d6469ca6b07f8f230bcb12a220e97
Mateus <mateus.frauzino@gmail.com>
2023-08-29 17:09:39 -0300

    index.js: changing homepage path

--- src/index.js
+++ src/index.js
@@ -3,11 +3,7 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import App from './App';
 import './index.css';
-import {
-    BrowserRouter,
-    Routes,
-    Route
-  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import QuemSomos from './pages/quem-somos/Quem-somos';
 import Contato from './pages/contato/Contato';
 import NossaProposta from './pages/nossa-proposta/Nossa-proposta';
@@ -15,18 +11,18 @@ import NossoEspaco from './pages/nosso-espaco/Nosso-espaco';
 import NossaEquipe from './pages/nossa-equipe/Nossa-equipe';
 import AgendeUmaVisita from './pages/agende-uma-visita/Agende-uma-visita';
 
-const rootElement = document.getElementById("root")
+const rootElement = document.getElementById('root');
 render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="QuemSomos" element={<QuemSomos />}/>
-            <Route path="Contato" element={<Contato />}/>
-            <Route path="NossaProposta" element={<NossaProposta />}/>
-            <Route path="NossoEspaco" element={<NossoEspaco />}/>
-            <Route path="NossaEquipe" element={<NossaEquipe />}/>
-            <Route path="AgendeUmaVisita" element={<AgendeUmaVisita />}/>
-        </Routes>
-    </BrowserRouter>,
-    rootElement
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path='/projeto-caliandra' element={<App />} />
+      <Route path='QuemSomos' element={<QuemSomos />} />
+      <Route path='Contato' element={<Contato />} />
+      <Route path='NossaProposta' element={<NossaProposta />} />
+      <Route path='NossoEspaco' element={<NossoEspaco />} />
+      <Route path='NossaEquipe' element={<NossaEquipe />} />
+      <Route path='AgendeUmaVisita' element={<AgendeUmaVisita />} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
+);
