efc8ccbba9f68875aed2a459f122c914bacdd1bd
Mateus <mateus.frauzino@gmail.com>
2023-08-29 18:55:32 -0300

    index: changing BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 import App from './App';
 import './index.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import QuemSomos from './pages/quem-somos/Quem-somos';
 import Contato from './pages/contato/Contato';
 import NossaProposta from './pages/nossa-proposta/Nossa-proposta';
@@ -13,22 +13,16 @@ import AgendeUmaVisita from './pages/agende-uma-visita/Agende-uma-visita';
 
 const rootElement = document.getElementById('root');
 render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
-      <Route path='/projeto-caliandra' element={<App />} />
-      <Route path='/projeto-caliandra/QuemSomos' element={<QuemSomos />} />
-      <Route path='/projeto-caliandra/Contato' element={<Contato />} />
-      <Route
-        path='/projeto-caliandra/NossaProposta'
-        element={<NossaProposta />}
-      />
-      <Route path='/projeto-caliandra/NossoEspaco' element={<NossoEspaco />} />
-      <Route path='/projeto-caliandra/NossaEquipe' element={<NossaEquipe />} />
-      <Route
-        path='/projeto-caliandra/AgendeUmaVisita'
-        element={<AgendeUmaVisita />}
-      />
+      <Route path='/' element={<App />} />
+      <Route path='QuemSomos' element={<QuemSomos />} />
+      <Route path='Contato' element={<Contato />} />
+      <Route path='NossaProposta' element={<NossaProposta />} />
+      <Route path='NossoEspaco' element={<NossoEspaco />} />
+      <Route path='NossaEquipe' element={<NossaEquipe />} />
+      <Route path='AgendeUmaVisita' element={<AgendeUmaVisita />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   rootElement
 );
