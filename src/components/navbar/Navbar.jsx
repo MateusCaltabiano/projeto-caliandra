4817a4b8570ab9d88f3d8220698475922ca5a459
Mateus <mateus.frauzino@gmail.com>
2023-08-29 18:54:48 -0300

    navbar: changing buttons to 'Link'

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,33 +2,20 @@ import React from 'react';
 import { RiMenu3Line, RiCLoseLin } from 'react-icons/ri';
 import logo from '../../assets/caliandra-logo.png';
 import './navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='caliandra__navbar'>
       <div className='caliandra__navbar-links'>
         <div className='caliandra__navbar-links_container'>
-          <p>
-            <a href='/projeto-caliandra'>Home</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/QuemSomos'>Quem somos</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/NossaProposta'>Nossa Proposta</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/NossoEspaco'>Nosso Espaço</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/NossaEquipe'>Nossa equipe</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/Contato'>Contato</a>
-          </p>
-          <p>
-            <a href='/projeto-caliandra/AgendeUmaVisita'>Agende uma visita</a>
-          </p>
+          <Link to='/'>Home</Link>
+          <Link to='/QuemSomos'>QuemSomos</Link>
+          <Link to='/NossaProposta'>Nossa Proposta</Link>
+          <Link to='/NossoEspaco'>Nosso Espaço</Link>
+          <Link to='/NossaEquipe'>Nossa Equipe</Link>
+          <Link to='/Contato'>Contato</Link>
+          <Link to='/AgendeUmaVisita'>Agende Uma Visita</Link>
         </div>
       </div>
     </div>
