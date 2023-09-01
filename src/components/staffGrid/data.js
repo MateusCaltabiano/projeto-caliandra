8cfb6403cdd13ab185eb40a34e3f741dcabeeaaa
Mateus <mateus.frauzino@gmail.com>
2023-09-01 17:06:53 -0300

    adding new asset

--- src/components/staffGrid/data.js
+++ src/components/staffGrid/data.js
@@ -1,20 +1,22 @@
+import logo from '../../assets/thisPersonDoesNotExist.jpeg';
+
 export const staffMembers = [
   {
-    photo: 'photoPath1',
+    photo: logo,
     name: 'Lorem ipsum',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius consequat sagittis. Duis sollicitudin quam eget massa interdum luctus. Ut non ante ligula. In a pretium nibh. Nulla luctus elit a gravida porttitor. Etiam eleifend eleifend elit, ac tempus neque faucibus vel. Integer cursus metus metus, quis fermentum felis maximus vitae. Donec ornare mi eu est ultrices, ut dignissim eros efficitur. Quisque luctus sodales erat id tempus. Sed vitae nibh enim.',
   },
 
   {
-    photo: 'photoPath2',
+    photo: logo,
     name: 'Lorem ipsum',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius consequat sagittis. Duis sollicitudin quam eget massa interdum luctus. Ut non ante ligula. In a pretium nibh. Nulla luctus elit a gravida porttitor. Etiam eleifend eleifend elit, ac tempus neque faucibus vel. Integer cursus metus metus, quis fermentum felis maximus vitae. Donec ornare mi eu est ultrices, ut dignissim eros efficitur. Quisque luctus sodales erat id tempus. Sed vitae nibh enim.',
   },
 
   {
-    photo: 'photoPath3',
+    photo: logo,
     name: 'Lorem ipsum',
     about:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius consequat sagittis. Duis sollicitudin quam eget massa interdum luctus. Ut non ante ligula. In a pretium nibh. Nulla luctus elit a gravida porttitor. Etiam eleifend eleifend elit, ac tempus neque faucibus vel. Integer cursus metus metus, quis fermentum felis maximus vitae. Donec ornare mi eu est ultrices, ut dignissim eros efficitur. Quisque luctus sodales erat id tempus. Sed vitae nibh enim.',
