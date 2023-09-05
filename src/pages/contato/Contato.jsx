5c29081f9ae5deae157bce21bc9b093d3b750dec
Mateus <mateus.frauzino@gmail.com>
2023-09-05 18:54:45 -0300

    contato: starting page development

--- src/pages/contato/Contato.jsx
+++ src/pages/contato/Contato.jsx
@@ -8,6 +8,12 @@ const Contato = () => {
     <div className='Contato'>
       <Header />
       <Navbar />
+      <div className='Contato_main'>
+        <a className='contato_whatsapp'>WhatsApp</a>
+        <a className='contato_instagram'>Instagram</a>
+        <a className='contato_email'>Email</a>
+        <a className='contato_telefone'>Telefone</a>
+      </div>
       <div className='footer-div'>
         <Footer />
       </div>
