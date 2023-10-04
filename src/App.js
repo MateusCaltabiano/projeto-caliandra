3d875aaa5ab95079111fd511718ad9749dc5ffcc
Mateus <mateus.frauzino@gmail.com>
2023-10-04 18:57:23 +0900

    Home/Quem-somos: adding 'in construction' text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 import { Footer, Header } from './containers';
 import { Navbar } from './components';
 import './App.css';
+import cone_icon from './assets/cone.png';
 
 const App = () => {
   return (
     <div className='App'>
       <Header />
       <Navbar />
+      <div className='home-page_main'>
+        <img src={cone_icon} />
+        <h1 className='home-page_h1'>Site em construção!</h1>
+        <p>
+          As páginas que já possuem conteúdo são: Nossa Proposta, Nosso Espaço e
+          Agende Uma Visita. Elas podem ser acessadas a partir da barra de
+          navegação no topo da página! Também é possível enviar uma mensagem por
+          WhatsApp e acessar nosso Instagram a partir dos botões no fim da
+          página.
+        </p>
+      </div>
       <div className='footer-div'>
         <Footer />
       </div>
--- src/pages/quem-somos/Quem-somos.jsx
+++ src/pages/quem-somos/Quem-somos.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './quem-somos.css';
 import { Header, Footer } from '../../containers';
 import { Navbar } from '../../components';
+import cone_icon from '../../assets/cone.png';
 
 const QuemSomos = () => {
   return (
     <div className='QuemSomos'>
       <Header />
       <Navbar />
+      <div className='QuemSomos_main'>
+        <img src={cone_icon}></img>
+      </div>
       <div className='footer-div'>
         <Footer />
       </div>
