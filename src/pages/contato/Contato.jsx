d465f9a3e3556a6d9556bf5e9fae14b198e5dad8
Mateus <mateus.frauzino@gmail.com>
2023-10-04 19:56:26 +0900

    Contato: changing page layout

--- src/pages/contato/Contato.jsx
+++ src/pages/contato/Contato.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './contato.css';
 import { Header, Footer } from '../../containers';
 import { Navbar } from '../../components';
+import whatsapp_icon from '../../assets/whatsapp_icon.png';
+import instagram_icon from '../../assets/instagram_icon.png';
+import email_icon from '../../assets/gmail.png';
+import telephone_icon from '../../assets/icons8-telephone-481.png';
 
 const Contato = () => {
   return (
@@ -9,10 +13,22 @@ const Contato = () => {
       <Header />
       <Navbar />
       <div className='Contato_main'>
-        <a className='contato_whatsapp'>WhatsApp</a>
-        <a className='contato_instagram'>Instagram</a>
-        <a className='contato_email'>Email</a>
-        <a className='contato_telefone'>Telefone</a>
+        <div className='contato_whatsapp'>
+          <img src={whatsapp_icon} />
+          <p>(61) 9812-2697</p>
+        </div>
+        <div className='contato_instagram'>
+          <img src={instagram_icon} />
+          <p>caliandra.escola</p>
+        </div>
+        <div className='contato_email'>
+          <img src={email_icon} alt='imagem_email' />
+          <p>caliandra.escola@gmail.com</p>
+        </div>
+        <div className='contato_telefone'>
+          <img src={telephone_icon} alt='imagem_telefone' />
+          <p>(61) 3034-2809</p>
+        </div>
       </div>
       <div className='footer-div'>
         <Footer />
