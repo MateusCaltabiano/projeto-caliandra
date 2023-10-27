cc4839d4393c43cdfcb16c6972981b2ca21e8890
Mateus <mateus.frauzino@gmail.com>
2023-10-27 18:08:30 +0900

    Nossas-turmas: adding main title and paragraph

--- src/pages/nossas-turmas/Nossas-turmas.jsx
+++ src/pages/nossas-turmas/Nossas-turmas.jsx
@@ -8,6 +8,24 @@ const NossasTurmas = () => {
     <div className='NossasTurmas'>
       <Header />
       <Navbar />
+      <div className='NossasTurmas_main'>
+        <h1>Nossas Turmas</h1>
+        <p>
+          Nossas salas possuem mobília de madeira feita sob medida para atender
+          às necessidades de cada fase do desenvolvimento da criança e criar um
+          ambiente belo e aconchegante. As salas possuem um piso de madeira
+          flutuante, que traz uma sensação térmica mais agradável e macia.
+        </p>
+        <br />
+        <p>
+          Além das salas de referência de cada turma, temos um ateliê onde
+          oferecemos atividades de artes em inglês e aulas de musicalização.
+          Temos um amplo pátio com uma estrurura de parquinho Pikleriano e um
+          mural sensorial interativo. Contamos também com um Parque de Areia,
+          uma área externa de Vida Prática com uma horta e uma linda
+          Brinquedoteca.
+        </p>
+      </div>
       <div className='footer-div'>
         <Footer />
       </div>
